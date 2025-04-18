624e58ce26b50f4835cd507c1ac45229e24f43ad
amindasoomi1 <amindasoomi1@gmail.com>
2025-04-18 15:59:31 +0330

    provider story

---
+++ src/providers/index.stories.tsx
@@ -0,0 +1,41 @@
+import { Button, Stack } from "@/components";
+import type { Meta } from "@storybook/react";
+import { ThemeProvider } from ".";
+
+const meta: Meta = {
+  title: "Providers",
+  parameters: { layout: "padded" },
+  tags: ["autodocs"],
+};
+
+export function Theme() {
+  return (
+    <Stack variant="vertical" className="items-start gap-4">
+      <Button variant="solid" color="primary">
+        Primary
+      </Button>
+      <Button variant="text" color="success">
+        Success
+      </Button>
+      <ThemeProvider
+        classes={{
+          button: {
+            base: "rounded-full",
+            color: {
+              primary: { solid: "text-dark" },
+              success: { text: "bg-success/10" },
+            },
+          },
+        }}
+      >
+        <Button variant="solid" color="primary">
+          Primary
+        </Button>
+        <Button variant="text" color="success">
+          Success
+        </Button>
+      </ThemeProvider>
+    </Stack>
+  );
+}
+export default meta;
