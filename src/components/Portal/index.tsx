ffaf8ecde1ec8620be1a9a613917fdc67b01ac72
amindasoomi1 <amindasoomi1@gmail.com>
2025-04-21 17:58:30 +0330

    debug portal

--- src/components/Portal/index.tsx
+++ src/components/Portal/index.tsx
@@ -1,10 +1,10 @@
 import { ChildrenProps } from "@/types";
-import { useLayoutEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 import { randomID } from "../../helpers";
 export default function Portal({ children }: ChildrenProps) {
   const divRef = useRef(document.createElement("div"));
-  useLayoutEffect(() => {
+  useEffect(() => {
     const div = divRef.current;
     const id = randomID();
     div.id = id;
