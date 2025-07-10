149f784997b412389c1de6def54c0d6e26ca94e0
amindasoomi1 <amindasoomi1@gmail.com>
2025-07-11 02:18:32 +0330

    debug use axios

--- src/axios/index.stories.tsx
+++ src/axios/index.stories.tsx
@@ -1,7 +1,5 @@
 import { Box, Button } from "@/components";
 import type { Meta } from "@storybook/react";
-import { InternalAxiosRequestConfig } from "axios";
-import { useCallback, useEffect } from "react";
 import AxiosProvider from "./AxiosProvider";
 import useAxios from "./useAxios";
 
@@ -19,9 +17,6 @@ function Component() {
       console.log(data);
     });
   };
-  useEffect(() => {
-    getData();
-  }, []);
   return (
     <Box>
       <Button type="button" loading={loading} onClick={getData}>
@@ -31,46 +26,9 @@ function Component() {
   );
 }
 export function Default() {
-  const accessToken = "";
-  const setToken = useCallback((token: unknown) => {
-    console.log(token);
-  }, []);
-  const handleRequestToken = useCallback(
-    (req: InternalAxiosRequestConfig) => {
-      if (accessToken) {
-        req.headers.set("Authorization", `Bearer ${accessToken}`);
-      }
-      return req;
-    },
-    [accessToken]
-  );
-  const handleUnauthorized = useCallback(
-    // eslint-disable-next-line
-    async (error: any) => {
-      const isUnauthorized = error?.response?.status === 401;
-      if (!isUnauthorized) return;
-      setToken(null);
-    },
-    [setToken]
-  );
-  const handleErrorMessage = useCallback(() => {
-    return "خطایی رخ داده است";
-  }, []);
-  const handleError = useCallback(
-    // eslint-disable-next-line
-    (error: any) => {
-      const isUnauthorized = error?.response?.status === 401;
-      const isCanceled = error?.code === "ERR_CANCELED";
-      const message = handleErrorMessage();
-      if (!isCanceled && !isUnauthorized) console.error(message);
-    },
-    [handleErrorMessage]
-  );
   return (
     <AxiosProvider
       config={{ baseURL: "https://jsonplaceholder.typicode.com" }}
-      beforeRequest={[handleRequestToken]}
-      afterError={[handleError, handleUnauthorized]}
       cancelOnUnmount
       cancelDuplicatedRequests
     >
