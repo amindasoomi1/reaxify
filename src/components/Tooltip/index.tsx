ca1a7be5c0cec2e4b90c4e3d95723425309e4fd9
Amin Dasoomi <amindasoomi1@gmail.com>
2025-11-24 23:18:47 +0330

    refactor tooltip

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -61,9 +61,10 @@ export default function Tooltip({
       "size-0 min-w-(--width) min-h-(--height) top-(--top) right-(--right) bottom-(--bottom) left-(--left) justify-end items-center p-1.5";
     const placements = {
       top: "-translate-y-full flex-col origin-bottom",
-      end: "translate-x-full flex-row-reverse origin-start",
+      end: "translate-x-[calc(var(--ratio)*100%)] flex-row-reverse origin-left rtl:origin-right",
       bottom: "translate-y-full flex-col-reverse origin-top",
-      start: "-translate-x-full flex-row origin-start",
+      start:
+        "translate-x-[calc(var(--ratio)*-100%)] flex-row origin-right rtl:origin-left",
     };
     return [base, placements[placement], classes?.placement?.[placement]];
   }, [placement, classes?.placement]);
@@ -118,8 +119,8 @@ export default function Tooltip({
     const height = rect.height;
     const top = rect.top;
     const left = rect.left;
-    const right = rect.right;
-    const bottom = rect.bottom;
+    const right = window.innerWidth - rect.right;
+    const bottom = window.innerHeight - rect.bottom;
 
     setPosition({
       "--width": `${width}px`,
@@ -150,14 +151,14 @@ export default function Tooltip({
               ref={divRef}
               style={position}
               className={cn(
-                "fixed flex justify-center items-center z-10 pointer-events-none transition-[scale,opacity]",
+                "fixed flex justify-center items-center z-10 pointer-events-none transition-[scale,opacity] [--ratio:1] rtl:[--ratio:-1]",
                 ...placementClasses,
                 transitionClasses[state]
               )}
             >
               <span
                 className={twMerge(
-                  "relative block w-fit h-fit bg-dark text-sm rounded px-2 py-px whitespace-nowrap z-[1]",
+                  "relative block size-fit min-w-fit min-h-fit bg-dark text-sm rounded px-2 py-px whitespace-nowrap z-[1]",
                   classes?.base,
                   colorClasses,
                   className
@@ -168,7 +169,7 @@ export default function Tooltip({
               </span>
               <span
                 className={twMerge(
-                  "block size-3 bg-dark rotate-45",
+                  "block size-3 min-w-3 min-h-3 bg-dark rotate-45",
                   arrowPlacementClasses,
                   colorClasses
                 )}
