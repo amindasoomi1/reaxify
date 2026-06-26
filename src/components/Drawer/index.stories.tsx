1640b5d4531fed98507850a5ab3ad34f76ef6594
Amin dasoomi <amindasoomi1@gmail.com>
2026-06-26 10:42:17 +0330

    debug prevent close

--- src/components/Drawer/index.stories.tsx
+++ src/components/Drawer/index.stories.tsx
@@ -1,9 +1,9 @@
 import type { Meta } from "@storybook/react";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Drawer from ".";
 import { useToggle } from "../..//hooks";
 import Button from "../Button";
-import Stack from "../Stack";
+import ButtonGroup from "../ButtonGroup";
 import Typography from "../Typography";
 
 const meta: Meta<typeof Drawer> = {
@@ -50,6 +50,7 @@ export function Default() {
 }
 
 export function Anchor() {
+  const anchors = ["top", "start", "bottom", "end"] as const;
   const [state, setState] = useState({
     top: false,
     start: false,
@@ -62,52 +63,54 @@ export function Anchor() {
     });
   };
   return (
-    <Stack wrap className="gap-2">
-      {(["top", "start", "bottom", "end"] as const).map((anchor) => (
-        <Fragment key={anchor}>
-          <Button
-            type="button"
-            onClick={() => toggleDrawer(anchor, true)}
-            className="capitalize"
-          >
-            {anchor}
-          </Button>
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={() => toggleDrawer(anchor, false)}
-          >
-            <Drawer.Menu>
-              <Drawer.Header>
-                <Typography variant="heading-6">Drawer header</Typography>
-              </Drawer.Header>
-              <Drawer.Body>
-                <Typography variant="body-1">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Consequatur atque debitis fugiat voluptatum facere, voluptate
-                  sapiente accusantium, dolor dicta mollitia corporis quas
-                  possimus omnis minus sequi, recusandae earum deserunt?
-                  Doloremque.
-                </Typography>
-              </Drawer.Body>
-              <Drawer.Footer className="flex items-center *:flex-1 gap-4">
-                <Button
-                  type="button"
-                  variant="outline"
-                  color="danger"
-                  closeDrawer
-                >
-                  Cancel
-                </Button>
-                <Button type="button" variant="solid" color="success">
-                  Accept
-                </Button>
-              </Drawer.Footer>
-            </Drawer.Menu>
-          </Drawer>
-        </Fragment>
+    <ButtonGroup>
+      {anchors.map((anchor) => (
+        <Button
+          key={anchor}
+          type="button"
+          onClick={() => toggleDrawer(anchor, true)}
+          className="capitalize"
+        >
+          {anchor}
+        </Button>
       ))}
-    </Stack>
+      {anchors.map((anchor) => (
+        <Drawer
+          key={anchor}
+          anchor={anchor}
+          open={state[anchor]}
+          onClose={() => toggleDrawer(anchor, false)}
+        >
+          <Drawer.Menu>
+            <Drawer.Header>
+              <Typography variant="heading-6">Drawer header</Typography>
+            </Drawer.Header>
+            <Drawer.Body>
+              <Typography variant="body-1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequatur atque debitis fugiat voluptatum facere, voluptate
+                sapiente accusantium, dolor dicta mollitia corporis quas
+                possimus omnis minus sequi, recusandae earum deserunt?
+                Doloremque.
+              </Typography>
+            </Drawer.Body>
+            <Drawer.Footer className="flex items-center *:flex-1 gap-4">
+              <Button
+                type="button"
+                variant="outline"
+                color="danger"
+                closeDrawer
+              >
+                Cancel
+              </Button>
+              <Button type="button" variant="solid" color="success">
+                Accept
+              </Button>
+            </Drawer.Footer>
+          </Drawer.Menu>
+        </Drawer>
+      ))}
+    </ButtonGroup>
   );
 }
 export function PreventClose() {
